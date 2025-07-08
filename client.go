a6935b01a2aee3966cc948e3d100045c9626961b
jirbthagoras <hansjabriel@gmail.com>
2025-07-09 06:10:39 +0700

    [+]feat/All: finished all feature

--- client.go
+++ client.go
@@ -16,35 +16,56 @@ type client struct {
 
 func (c *client) readInput() {
 	for {
-		// reads all the message sent by user via the peer connection
 		msg, err := bufio.NewReader(c.conn).ReadString('\n')
 		if err != nil {
 			return
 		}
 
-		// processes the message
 		msg = strings.Trim(msg, "\r\n")
 
 		args := strings.Split(msg, " ")
-
 		cmd := strings.TrimSpace(args[0])
 
 		switch cmd {
 		case "/nick":
-		case "/rooms":
+			c.commands <- command{
+				id:     CMD_NICK,
+				client: c,
+				args:   args,
+			}
 		case "/join":
+			c.commands <- command{
+				id:     CMD_JOIN,
+				client: c,
+				args:   args,
+			}
+		case "/rooms":
+			c.commands <- command{
+				id:     CMD_ROOMS,
+				client: c,
+			}
 		case "/msg":
+			c.commands <- command{
+				id:     CMD_MSG,
+				client: c,
+				args:   args,
+			}
 		case "/quit":
+			c.commands <- command{
+				id:     CMD_QUIT,
+				client: c,
+			}
 		default:
-			c.err(fmt.Errorf("unknow command %s", cmd))
+			c.err(fmt.Errorf("unknown command: %s", cmd))
 		}
 	}
 }
 
 func (c *client) err(err error) {
-	c.conn.Write([]byte("ERR: " + err.Error() + "\n"))
+	c.conn.Write([]byte("err: " + err.Error() + "\n"))
 }
 
 func (c *client) msg(msg string) {
-	c.conn.Write([]byte("> " + msg + "\func"))
+	c.conn.Write([]byte("> " + msg + "\n"))
 }
+
--- main.go
+++ main.go
@@ -6,26 +6,26 @@ import (
 )
 
 func main() {
-	// creates a new server instance
 	s := newServer()
+	go s.run()
 
-	// listens the tcp port
 	listener, err := net.Listen("tcp", ":8888")
 	if err != nil {
-		log.Fatalf("unable to start the server: %s", err.Error())
+		log.Fatalf("unable to start server: %s", err.Error())
 	}
 
-	// dont forget to close the listener
 	defer listener.Close()
-	log.Printf("server listened to port: 8888")
+	log.Printf("server started on :8888")
 
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			log.Printf("unable to accept connection: %s", err.Error())
+			log.Printf("failed to accept connection: %s", err.Error())
 			continue
 		}
 
-		go s.newClient(conn)
+		c := s.newClient(conn)
+		go c.readInput()
 	}
 }
+
